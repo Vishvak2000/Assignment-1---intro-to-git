a7e4adc37e06430087195479d17b92405eac1b74
Vishvak <vishvak2000@gmail.com>
2020-04-26 16:19:00 -0700

    Merge Conflict ***

--- Project2/siete-y-medio.cpp
+++ Project2/siete-y-medio.cpp
@@ -8,12 +8,25 @@
 using namespace std;
 
 // Global constants (if any)
-
+int N_GAMES = 1;
 
 // Non member functions declarations (if any)
-
+void print_info(Player* player, Player* dealer);
 // Non member functions implementations (if any)
+void print_info(Player* player, Player* dealer) {
+   
+    cout << "---------------------------" << endl << endl;
+    cout << "Game Number: " << N_GAMES << endl << "Your money : " << player->get_money() << endl << endl;
 
+    cout << "Your cards: " << endl;
+     player->get_hand()->print_hand();
+    cout << "Your total: " << player->get_hand()->get_total() << endl << endl;
+
+    cout << "Dealer's cards: " << endl;
+    dealer->get_hand()->print_hand();
+    cout << "Your total: " << dealer ->get_hand()->get_total()<< endl << endl;
+    
+}
 
 // Stub for main
 int main() {
